c5cd26a1c0c3c5fe8d219184b88874a0080d14c6
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-10 22:05:58 +0000

    forgot to set up a repo for the project until now, these are the commits so far

---
+++ express_server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`); // response
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = ...
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+// functions
+// random string - sampled from https://stackoverflow.com/questions/16106701/how-to-generate-a-random-string-of-letters-and-numbers-in-javascript
+function generateRandomString() {
+  let string = "";
+  var charset = "0123456789abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < 6; i++) {
+    string += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return string;
+}
