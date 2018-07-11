72c106b94481c886ba8bc0dbb0a1b4e04e9f2aad
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-11 22:25:09 +0000

    added login cookie

--- express_server.js
+++ express_server.js
@@ -2,33 +2,46 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/views')); // allows css in expressjs
+app.use(express.static(__dirname + "/views")); // allows css in expressjs
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// home redirect to urls page
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
+// create login cookie, redirect to urls page
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+// render urls page
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// render error page
 app.get("/urls/error", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.status(400).render("urls_error", templateVars);
 });
 
+// render new url page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// create new short/long url pair, redirect to urls
 app.post("/urls/new", (req, res) => {
   let shortURL = generateRandomString();
   let longURL = req.body.longURL;
@@ -38,7 +51,7 @@ app.post("/urls/new", (req, res) => {
   }
   if (!longURL.includes("www.")) {
     checker = false;
-    res.status(400).redirect('/urls/error'); // send to error page,
+    res.status(400).redirect("/urls/error"); // send to error page,
   }
   if (checker) {
     if (!longURL.startsWith("http")) {
@@ -52,27 +65,30 @@ app.post("/urls/new", (req, res) => {
   }
 });
 
+// redirect to full site
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   if (urlDatabase[req.params.shortURL]) { // check if shortURL is in the database
     res.redirect(longURL); // redirect to longURL
   } else {
-    res.status(404).redirect('/urls/error'); // send to error page
+    res.status(404).redirect("/urls/error"); // send to error page
   }
 });
 
+// render show page
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
+// edit url, redirect to urls page
 app.post("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
   let longURL = req.body[req.params.id];
   let checker = true;
   if (!longURL.includes("www.")) {
     checker = false;
-    res.status(400).redirect('/urls/error'); // send to error page,
+    res.status(400).redirect("/urls/error"); // send to error page,
   }
   if (checker) {
     if (!longURL.startsWith("http")) {
@@ -86,16 +102,19 @@ app.post("/urls/:id", (req, res) => {
   }
 });
 
+// delete url, redirect to urls page
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
+// redirect edit to show page
 app.get("/urls/:id/edit", (req, res) => {
   let shortURL = req.params.id;
   res.redirect(`/urls/${shortURL}`);
 });
 
+// show json file
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
